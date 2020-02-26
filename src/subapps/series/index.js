5a1171d979cedea04a04059f79431e4e685ff3ba
Shlok Desai <shlokpdesai@gmail.com>
2020-02-25 17:32:46 -0800

    add loading indicator

--- src/subapps/series/index.js
+++ src/subapps/series/index.js
@@ -5,33 +5,45 @@ export default class extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      series: []
+      series: [],
+      uiState: "DONE"
     };
   }
 
   componentDidMount() {
-    fetch(
-      "https://raw.githubusercontent.com/StreamCo/react-coding-challenge/master/feed/sample.json"
-    )
-      .then(response => response.json())
-      .then(responseJSON => {
-        const series = responseJSON.entries.filter(
-          entry => entry.programType === "series"
-        );
-        this.setState({
-          series
-        });
-      })
-      .catch(err => {
-        this.setState({
-          error: true
-        });
+    if (this.state.series.length === 0) {
+      this.setState({
+        uiState: "LOADING"
       });
+      fetch(
+        "https://raw.githubusercontent.com/StreamCo/react-coding-challenge/master/feed/sample.json"
+      )
+        .then(response => response.json())
+        .then(responseJSON => {
+          const series = responseJSON.entries.filter(
+            entry => entry.programType === "series"
+          );
+          this.setState({
+            series,
+            uiState: "DONE"
+          });
+        })
+        .catch(err => {
+          this.setState({
+            uiState: "DONE"
+          });
+        });
+    }
   }
 
   render() {
+    const isLoading = this.state.uiState === "LOADING";
+
     return (
       <div className="flex flex-wrap">
+        <p className={`${isLoading ? "opacity-100" : "opacity-0"} p-4 text-lg`}>
+          Loading...
+        </p>
         {this.state.series.map((series, index) => (
           <InfoCard key={`series-${index}`} {...series} />
         ))}
